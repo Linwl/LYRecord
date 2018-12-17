478bffab030c6e139c92057789774f260f39c74a
Linwl <304115325@qq.com>
2018-12-17 14:59:30 +0800

    增加启动文件的命令输入

--- LYRecord_py/app.py
+++ LYRecord_py/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import argparse
 
 app = Flask(__name__)
 
@@ -9,4 +10,9 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    parser = argparse.ArgumentParser(description='start the web service!')
+    parser.add_argument('--host', '-i',default='0.0.0.0',type=str, help='input web ip address')
+    parser.add_argument('--port', '-p', required=True,type=int, help='input web port')
+    args = parser.parse_args()
+    app.run(host=args.host, port=args.port, debug=False,use_reloader=False)
+    # app.run()
