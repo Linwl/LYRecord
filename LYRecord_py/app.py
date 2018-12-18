c0c7cefa71b84e245fb598d281469c633c6460d9
Linwl <304115325@qq.com>
2018-12-18 09:07:00 +0800

    去除pipenv管理

--- LYRecord_py/app.py
+++ LYRecord_py/app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import argparse
 
 app = Flask(__name__)
 
@@ -15,4 +14,3 @@ if __name__ == '__main__':
     parser.add_argument('--port', '-p', required=True,type=int, help='input web port')
     args = parser.parse_args()
     app.run(host=args.host, port=args.port, debug=False,use_reloader=False)
-    # app.run()
