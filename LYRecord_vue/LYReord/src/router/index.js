97c78bafe1da67c4160a07061f43f6ed0e592471
Linwl <304115325@qq.com>
2019-02-06 17:17:02 +0800

    初始化路由

--- LYRecord_vue/LYReord/src/router/index.js
+++ LYRecord_vue/LYReord/src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
 Vue.use(Router)
 
+import Layout from '@/views/layout/Layout'
+
+export const constantRouterMap = [
+  { path: '/login', component: () => import('@/views/login/index'), hidden: true },
+  { path: '/404', component: () => import('@/views/404'), hidden: true },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    name: 'Dashboard',
+    hidden: true,
+    children: [{
+      path: 'dashboard',
+      component: () => import('@/views/dashboard/index')
+    }]
+  },
+  { path: '*', redirect: '/404', hidden: true }
+]
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  // mode: 'history', //后端支持可开
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
 })
---
+++ LYRecord_vue/LYReord/src/views/login/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      登录
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+
+</style>
